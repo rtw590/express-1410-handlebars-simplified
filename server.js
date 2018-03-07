c2e3247ffc328fed7b1b09e146b66dde6d80bbe6
Rob Waller <r.t.waller@gmail.com>
2018-03-07 13:54:50 -0600

    basic site structure created

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var people = ['Rob', 'John', 'Mike'];
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/tutorials', function(req, res){
+    res.render('tutorials');
+});
+
+app.get('/freelance', function(req, res){
+    res.render('freelance');
+});
+
+app.get('/store', function(req, res){
+    res.render('store');
+});
+
+var port = process.env.PORT || 3000;
+  
+app.listen(port, function () {
+    console.log('Server listening at port', port);
+}); 
