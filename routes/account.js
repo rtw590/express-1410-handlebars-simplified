e049f0a13694307f77a60206435fc4a3b6b136ce
Rob Waller <r.t.waller@gmail.com>
2018-03-16 13:54:04 -0500

    Sign up and Login both working

--- routes/account.js
+++ routes/account.js
@@ -23,4 +23,15 @@ router.get('/profile', function(req, res, next) {
     res.render('user/profile');
 });
 
+router.get('/signin', function(req, res, next) {
+    var messages = req.flash('error');
+    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/signin', passport.authenticate('local.signin', {
+    successRedirect: 'profile',
+    failureRedirect: 'signin',
+    failureFlash: true
+}));
+
 module.exports = router;
