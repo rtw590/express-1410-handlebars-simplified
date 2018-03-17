e89e5ad3dcce00a10aea1bcb8746420572c1f313
Rob Waller <r.t.waller@gmail.com>
2018-03-17 08:44:22 -0500

    adding item to cart is causing error. Video 12, 7 minutes in

--- routes/tutorials.js
+++ routes/tutorials.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Tutorial = require('../models/tutorial');
+var Cart = require('../models/cart');
 
 router.use(express.static('public'))
 
@@ -22,6 +23,22 @@ router.get('/:id', function(req, res){
             tutorial: tutorial
         });
     })
+});
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
+
+    Tutorial.findById(req.params.id, function(err, tutorial) {
+        if (err) {
+            console.log(err);
+            return res.send('There was an error');
+        }
+        cart.add(tutorial, tutorial.id);
+        res.session.cart = cart;
+        console.log(req.session.cart)
+        res.redirect('/')
+    });
 });
 
 module.exports = router;
